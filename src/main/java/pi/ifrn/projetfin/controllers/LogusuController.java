9afd34d1b1e183683d4e76292241830716bb03f6
jhenyffer <jhenyfferviana2@gmail.com>
2023-12-26 16:25:39 -0300

    Criando pagina e controller de login de usuario

---
+++ src/main/java/pi/ifrn/projetfin/controllers/LogusuController.java
@@ -0,0 +1,13 @@
+package pi.ifrn.projetfin.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LogusuController {
+
+	@RequestMapping("/recycle/logusu")
+	public String Logusu() {
+		return "logusua";
+	}
+}
