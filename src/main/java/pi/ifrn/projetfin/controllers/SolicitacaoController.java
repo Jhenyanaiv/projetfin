e527adb871a8b38137705ccf3aa71992b3cf6324
jhenyffer <jhenyfferviana2@gmail.com>
2023-12-26 15:29:24 -0300

    criando parte de solicitação

--- src/main/java/pi/ifrn/projetfin/controllers/SolicitacaoController.java
+++ src/main/java/pi/ifrn/projetfin/controllers/SolicitacaoController.java
@@ -1,10 +1,12 @@
 package pi.ifrn.projetfin.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SolicitacaoController {
 
+	@RequestMapping("/recycle/solic")
 	public String solic() {
 		return "formsolic";
 	}
---
+++ src/main/java/pi/ifrn/projetfin/models/solic.java
@@ -0,0 +1,35 @@
+package pi.ifrn.projetfin.models;
+
+public class solic {
+	
+	private String nome;
+	private String endereco;
+	private String email;
+	private String descricao;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
