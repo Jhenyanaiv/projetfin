2a9acc19e8c885ce1f041e6b3d2c878d102186d3
jhenyffer <jhenyfferviana2@gmail.com>
2023-12-26 14:40:28 -0300

    Iniciando aplicação

---
+++ src/test/java/pi/ifrn/projetfin/ProjetfinApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.ifrn.projetfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetfinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
