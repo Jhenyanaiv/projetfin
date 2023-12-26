78099068a9ae9c3a33b5570359cd701fb5c9c0c3
jhenyffer <jhenyfferviana2@gmail.com>
2023-12-26 15:07:36 -0300

    Criando controller e html da solicitação

---
+++ src/main/java/pi/ifrn/projetfin/controllers/SolicitacaoController.java
@@ -0,0 +1,11 @@
+package pi.ifrn.projetfin.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SolicitacaoController {
+
+	public String solic() {
+		return "formsolic";
+	}
+}
