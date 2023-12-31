8195d077bccf8667a8e2bc910b57112d2dd0e025
Karol Sabino <elayne.sabino@escolar.ifrn.edu.br>
2023-12-30 22:58:23 -0300

    configurando listagem mas sem sucesso ainda

--- src/main/java/pi/ifrn/projetfin/controllers/SolicitacaoController.java
+++ src/main/java/pi/ifrn/projetfin/controllers/SolicitacaoController.java
@@ -1,29 +1,47 @@
 package pi.ifrn.projetfin.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import pi.ifrn.projetfin.models.solic;
 import pi.ifrn.projetfin.repositories.SolicRepository;
 
 @Controller
+@RequestMapping("/recycle")
 public class SolicitacaoController {
 
 	@Autowired
 	private SolicRepository sr;
 	
-	@RequestMapping("/recycle/solicitacao")
+	@RequestMapping("/solicitacao")
 	public String solic() {
 		return "solic/formSolic";
 	}
 	
-	@PostMapping("/recycle")
-	public String adicionarsolic(solic solic) {
-		
+	@PostMapping("/adicionar")
+	public String adicionarSolic(solic solic) {
+		try {
 		System.out.println(solic);
 		sr.save(solic);
 		return "solic/test1"; 
+	} catch (Exception e) {
+        e.printStackTrace(); 
+        return "errorPage";
+	}}
+	
+	@GetMapping
+	public ModelAndView listar() {
+		
+		List<solic> recycle = sr.findAll();
+		ModelAndView mv = new ModelAndView("solic/listsol");
+		mv.addObject("listsol", recycle);
+		return mv;
+		
 	}
 }
